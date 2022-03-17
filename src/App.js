6813b7866e1b089e0c7a0b529bf2c1bad6123e5e
RAFAEL NASCIMENTO <90287859+rafelradan@users.noreply.github.com>
2022-03-17 08:31:44 -0300

    First commit app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import cross from 'cross';
+import { useState } from 'react';
 
 function App() {
+  const [urlYT, setUrlYT] = useState('')
+
+    const UrlObj = {
+      urlYT : urlYT
+    }
+   function getUrlYT(e) {
+    e.preventDefault()
+    if (urlYT === '') {
+      alert('É necessário uma URL de vídeo do YouTube!')
+    } else{
+      //axios.post('https://backdlty.herokuapp.com/', UrlObj)
+      //https://backdlty.herokuapp.com/
+      axios.post('http://localhost:3333/', UrlObj)
+      console.log(UrlObj);
+      
+    }
+  }
+
+  function clearUrlYT() {
+    setUrlYT('')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form method='get'>
+          <h1>Baixar vídeos do YouTube</h1>
+          
+          <div className='container'>
+            <h2>Cole a URL do vídeo</h2>
+            <input className='inpUrl' placeholder='url do vídeo' value={urlYT} onChange={e => setUrlYT(e.target.value)} />
+            <button className='download' onClick={getUrlYT}>Download</button>
+          </div>
+
+        </form>
       </header>
     </div>
   );
